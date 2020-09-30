bddb022166b6eb7c7fc2262c80ad9547c3a8c97c
uyen <uyen@codegym.vn>
2020-09-30 21:42:42 +0700

    eat apple

---
+++ js/Apple.js
@@ -0,0 +1,17 @@
+class Apple {
+    constructor() {
+        this.weight=10;
+
+    }
+    decrease(){
+        return this.weight--;
+    }
+    isEmpty() {
+        return this.weight === 0
+
+    }
+    getWeight(){
+        return this.weight;
+    }
+
+}
---
+++ js/Human.js
@@ -0,0 +1,38 @@
+class Human{
+    constructor(name,gender,weight) {
+        this.name=name;
+        this.gender=gender;
+        this.weight=weight;
+    }
+    isMale(){
+        if (this.gender == "male") {
+            return true
+        } else {
+            return false
+        }
+    }
+    setGender(boolean){
+        return this.gender = set;
+    }
+
+    eatApple(){
+        if (apple.weight > 0) {
+            apple.weight--;
+            this.weight++;
+            console.log(this.name + " ăn táo");
+        } else {
+            console.log()
+        }
+    }
+
+    getName(String){
+        return this.name;
+    }
+
+    getWeight(){
+        return this.weight;
+
+    }
+
+}
+
